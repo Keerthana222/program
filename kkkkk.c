791878fd86633077f6c3fedfb208cf6739c1c903
Keerthana222 <keerthisri22296@gmail.com>
2016-06-28 11:29:37 +0000

    Add files via upload

--- kkkkk.c
+++ kkkkk.c
@@ -3,9 +3,9 @@
 int  main()
 {char t;
 char str[100]="t";
-int i,j=0,temp;
+int i,j=0;
 printf("\n enter the string");
-grts(str);
+gets(str);
 i=0;j=strlen(str)-1;
 while(i<j)
 {
@@ -14,7 +14,8 @@ str[i]=str[j];
 str[j]=t;
 i++;
 j--;
-printf("\n reverse the string is %s",str)
+}
+printf("\n reverse the string is %s",str);
 return 0;
 }
-}
+
